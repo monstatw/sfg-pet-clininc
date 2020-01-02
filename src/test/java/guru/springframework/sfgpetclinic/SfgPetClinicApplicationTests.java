3100b25b9302a572898f32b79dea176428c95ef8
Johnson Chuang <johnsonchuang@exosite.com>
2020-01-02 11:46:37 +0800

    Rename wrong package name

--- src/test/java/guru/springframework/sfgpetclinic/SfgPetClinicApplicationTests.java
+++ src/test/java/guru/springframework/sfgpetclinic/SfgPetClinicApplicationTests.java
@@ -1,4 +1,4 @@
-package guru.springframeworl.sfgpetclinic;
+package guru.springframework.sfgpetclinic;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
